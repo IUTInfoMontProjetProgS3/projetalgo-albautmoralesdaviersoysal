1652c3462e6d3b78cb60e9d86c16a8acbbeb59a3
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-13 14:00:13 +0100

    📝 Mise à jour JavaDoc : Coord

--- src/main/java/fr/umontpellier/iut/algogen/Coord.java
+++ src/main/java/fr/umontpellier/iut/algogen/Coord.java
@@ -2,15 +2,29 @@ package fr.umontpellier.iut.algogen;
 
 import java.util.Objects;
 
+/**
+ * <b>Coord est la classe représentant une coordonnée sur le plateau de jeu.</b>
+ * <p>
+ * Une instance du Coord est caractérisé par les informations suivantes :
+ * <ul>
+ * <li>Un numéro de ligne qui indique sur quelle ligne il se trouve.</li>
+ * <li>Un numéro de colonne qui indique sur quelle colonne il se trouve.li>
+ * </ul>
+ * </p>
+ * 
+ * @version 1.0
+ */
 public class Coord {
-    private final int l = 0;
-    private final int c = 0;
+    private final int ligne = 0;
+    private final int colonne = 0;
 
-    public Coord(int nbLinge, int nbColonne) {
+    public Coord(int ligne, int colonne) {
 
     }
 
     /**
+     * Vérifie si this peut être dans une grille avec {@code nbLigne} et {@code nbColonne}.
+     * 
      * @param nbLigne : nombre de lignes de la grille
      * @param nbColonne : nombre de colonnes de la grille
      * @return true si la coordonnée ne dépace pas les bordures de la grille
@@ -21,25 +35,30 @@ public class Coord {
     }
 
     /**
-     * @param coord : Coordonnée
-     * @return true si this est de distance 1 de la coordonnée cd
+     * Vérifie si la {@code Coord} donné en paramétre est adjacent à this.
+     * 
+     * @param coordCible : Coordonnée dont on va vérifié la distance
+     * @return {@code true} si this est de distance 1 de la coordonnée coord
      * 
      **/
-    public boolean estADistanceUn(Coord coord) {
+    public boolean estADistanceUn(Coord coordCible) {
         return false;
     }
 
+
     /**
-     * @param coord : Coordonnée
-     * @return distance entre this et la coordonnée cd
+     * Renvoi la distance entre this et {@code coordCible} donné en paramètre
+     * 
+     * @param coordCible : Coordonnée ciblé
+     * @return {@code int} distance entre this et la coordonnée ciblé {@code coordCible}
      * 
      **/
-    public int distanceFrom(Coord coord) {
+    public int distanceFrom(Coord coordCible) {
         return 0;
     }
 
     public String toString() {
-        return "(" + l + "," + c + ")";
+        return "(" + ligne + "," + colonne + ")";
     }
 
     @Override
@@ -49,19 +68,34 @@ public class Coord {
         if (o == null || getClass() != o.getClass())
             return false;
         Coord coord = (Coord) o;
-        return l == coord.l && c == coord.c;
+        return ligne == coord.ligne && colonne == coord.colonne;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(l, c);
+        return Objects.hash(ligne, colonne);
     }
 
+    /**
+     * Retourne le numéro de ligne de this.
+     * 
+     * @return un {@code int} qui correspond au numéro de ligne de this.
+     * 
+     * @see Coord#ligne
+     */
     public int getL() {
         // TODO Auto-generated method stub
         return 0;
     }
 
+
+    /**
+     * Retourne le numéro de colonne de this.
+     * 
+     * @return un {@code int} qui correspond au numéro de colonne de this.
+     * 
+     * @see Coord#colonne
+     */
     public int getC() {
         // TODO Auto-generated method stub
         return 0;
