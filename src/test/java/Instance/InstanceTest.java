262995d7508dfffd1032f3c5b815f3ecc993746b
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-20 22:53:01 +0100

    ✅ Ajout test non-validé : GreedySolver

--- src/test/java/Instance/InstanceTest.java
+++ src/test/java/Instance/InstanceTest.java
@@ -200,6 +200,7 @@ public class InstanceTest {
                 assertTrue(instance.estValide(instance.greedySolver()));
         }
 
+        @Ignore("Non validé")
         @Test
         public void testGreedySolver() throws Exception {
                 boolean[][] plateau = new boolean[][] { // Disposition des pièces :
@@ -231,9 +232,9 @@ public class InstanceTest {
                 assertEquals(resultaAttendu, instance.greedySolver());
         }
 
-        @Ignore("Nom validé")
+        @Ignore("Non validé")
         @Test
-        void testGreedySolver_SolutionValideQuandPieceTropLoin() throws Exception {
+        public void testGreedySolver_SolutionValideQuandPieceTropLoin() throws Exception {
                 boolean[][] plateau = new boolean[][] { // Disposition des pièces :
                                 { false, true, false, false, false }, // l0: . o . . .
                                 { false, true, true, false, false }, // ,l1: . o o . .
@@ -253,9 +254,9 @@ public class InstanceTest {
                 assertTrue(instance.estValide(instance.greedySolver()));
         }
 
-        @Ignore("Nom validé")
+        @Ignore("Non validé")
         @Test
-        void testGreedySolver_BonneSolutionQuandPieceTropLoin() throws Exception {
+        public void testGreedySolver_BonneSolutionQuandPieceTropLoin() throws Exception {
                 boolean[][] plateau = new boolean[][] { // Disposition des pièces :
                                 { false, true, false, false, true }, // ,,l0: . o . . o
                                 { false, false, false, false, false }, // l1: . . . . .
@@ -280,7 +281,39 @@ public class InstanceTest {
                 assertEquals(resultaAttendu, instance.greedySolver());
         }
 
-        @Ignore
+        @Ignore("Non validé")
+        @Test
+        public void testGreedySolver_casSpecial() throws Exception {
+                boolean[][] plateau = new boolean[][] { // Disposition des pièces :
+                                { false, false, true, true, true }, // ,,,l0: . . o o o
+                                { false, false, false, false, false }, // l1: . . . . .
+                                { false, false, false, false, false }, // l2: . . . . .
+                                { false, false, false, false, false }, // l3: . . . . .
+                                { false, false, false, false, true } // ,,l4: . . . . o
+                };
+                Coord coordDepart = new Coord(0, 0);
+                int k = 8;
+                Instance instance = new Instance(plateau, coordDepart, k);
+                Solution resultaAttendu = new Solution();
+                // resultat attendu :
+                // l0: x x x x x
+                // l1: . . . . x
+                // l2: . . . . x
+                // l3: . . . . x
+                // l4: . . . . x
+                resultaAttendu.add(new Coord(0, 0));
+                resultaAttendu.add(new Coord(0, 1));
+                resultaAttendu.add(new Coord(0, 2));
+                resultaAttendu.add(new Coord(0, 3));
+                resultaAttendu.add(new Coord(0, 4));
+                resultaAttendu.add(new Coord(1, 4));
+                resultaAttendu.add(new Coord(2, 4));
+                resultaAttendu.add(new Coord(3, 4));
+                resultaAttendu.add(new Coord(4, 4));
+                assertEquals(resultaAttendu, instance.greedySolver());
+        }
+
+        @Ignore("Non validé")
         @Test
         public void testGreedyPermut() throws Exception {
                 boolean[][] p4 = new boolean[10][10];
