d880ea664145a60c41823c651d3d8ef955b0be30
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-20 11:17:27 +0100

    ✅ Ajout test non-validé greedySolver

--- src/test/java/Instance/InstanceTest.java
+++ src/test/java/Instance/InstanceTest.java
@@ -231,6 +231,55 @@ public class InstanceTest {
                 assertEquals(resultaAttendu, instance.greedySolver());
         }
 
+        @Ignore("Nom validé")
+        @Test
+        void testGreedySolver_SolutionValideQuandPieceTropLoin() throws Exception {
+                boolean[][] plateau = new boolean[][] { // Disposition des pièces :
+                                { false, true, false, false, false }, // l0: . o . . .
+                                { false, true, true, false, false }, // ,l1: . o o . .
+                                { false, false, true, true, false }, // ,l2: . . o o .
+                                { false, false, false, true, true }, // ,l3: . . . o o
+                                { false, false, false, false, true } // ,l4: . . . . o
+                };
+                Coord coordDepart = new Coord(0, 0);
+                int k = 3;
+                Instance instance = new Instance(plateau, coordDepart, k);
+                // resultat attendu :
+                // l0: x x . . .
+                // l1: . x x . .
+                // l2: . . . . .
+                // l3: . . . . .
+                // l4: . . . . .
+                assertTrue(instance.estValide(instance.greedySolver()));
+        }
+
+        @Ignore("Nom validé")
+        @Test
+        void testGreedySolver_BonneSolutionQuandPieceTropLoin() throws Exception {
+                boolean[][] plateau = new boolean[][] { // Disposition des pièces :
+                                { false, true, false, false, true }, // ,,l0: . o . . o
+                                { false, false, false, false, false }, // l1: . . . . .
+                                { false, false, false, false, false }, // l2: . . . . .
+                                { false, false, false, false, false }, // l3: . . . . .
+                                { false, false, false, false, false } // ,l4: . . . . .
+                };
+                Coord coordDepart = new Coord(0, 0);
+                int k = 3;
+                Instance instance = new Instance(plateau, coordDepart, k);
+                Solution resultaAttendu = new Solution();
+                // resultat attendu :
+                // l0: x x x x .
+                // l1: . . . . .
+                // l2: . . . . .
+                // l3: . . . . .
+                // l4: . . . . .
+                resultaAttendu.add(new Coord(0, 0));
+                resultaAttendu.add(new Coord(0, 1));
+                resultaAttendu.add(new Coord(0, 2));
+                resultaAttendu.add(new Coord(0, 3));
+                assertEquals(resultaAttendu, instance.greedySolver());
+        }
+
         @Ignore
         @Test
         public void testGreedyPermut() throws Exception {
