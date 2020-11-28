7b787d329532d803584d7fb557b4b03e9f47cc97
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-11-28 15:20:51 +0100

    ✅ Ajout mock test-validé

--- src/main/java/fr/umontpellier/iut/algogen/AlgoGenetique.java
+++ src/main/java/fr/umontpellier/iut/algogen/AlgoGenetique.java
@@ -1,6 +1,8 @@
 package fr.umontpellier.iut.algogen;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import fr.umontpellier.iut.algogen.fabriques.ICreator;
 import fr.umontpellier.iut.algogen.individus.IIndividu;
@@ -10,7 +12,8 @@ import fr.umontpellier.iut.algogen.strategies.StrategieCalculNextGen;
 /**
  * <b>AlgoGenetique est la classe représentant un système de résolution.</b>
  * <p>
- * Une instance de AlgoGenetique est caractérisé par les informations suivantes :
+ * Une instance de AlgoGenetique est caractérisé par les informations suivantes
+ * :
  * <ul>
  * <li>Une {@code Instance}.</li>
  * <li>Un {@code ICreator} createur d'individu.</li>
@@ -18,12 +21,12 @@ import fr.umontpellier.iut.algogen.strategies.StrategieCalculNextGen;
  * </ul>
  * </p>
  * <p>
- * De plus, un AlgoGenetique est une liste d'objet qui extend {@link IIndividu}. 
+ * De plus, un AlgoGenetique est une liste d'objet qui extend {@link IIndividu}.
  * </p>
  *
  * @see IIndividu
  *
- * @version 1.0
+ * @version 1.0.1
  */
 class AlgoGenetique<T extends IIndividu<T>> {
 
@@ -32,8 +35,8 @@ class AlgoGenetique<T extends IIndividu<T>> {
     private ICreator<T> createurIndividu;
 
     /**
-     * @param instance : instance ,
-     * @param calculGen : strategie de calcul des nouvelles generations
+     * @param instance         : instance
+     * @param calculGen        : strategie de calcul des nouvelles generations
      * @param createurIndividu : createur d'individus
      *
      */
@@ -44,36 +47,31 @@ class AlgoGenetique<T extends IIndividu<T>> {
     }
 
     /**
-     * @param instance : instance ,
+     * @param instance         : instance
      * @param createurIndividu : createur d'individus
      *
      */
     public AlgoGenetique(Instance instance, ICreator<T> createurIndividu) {
         this.instance = instance;
-        calculGen = new CroisementMutationV1<>();
+        calculGen = new CroisementMutationV1<>(0.1);
         this.createurIndividu = createurIndividu;
     }
 
     /**
-     * Créer population initiale Lancer Strategie de calcule de nouvelles generations nbGen fois
-     * @param taillePop : taille de la Population ,
-     * @param nbGeneration     : nombre de generations
+     * Créer population initiale Lancer Strategie de calcule de nouvelles
+     * generations nbGen fois
+     * 
+     * @param taillePop    : taille de la Population
+     * @param nbGeneration : nombre de generations
      * @return retourne la meilleur solution
      *
      */
     public Solution run(int taillePop, int nbGeneration) {
-        ArrayList<T> population  = createurIndividu.creerPopInit(instance, taillePop);
+        ArrayList<T> population = createurIndividu.creerPopInit(instance, taillePop);
         for (int i = 0; i < nbGeneration; i++) {
             population = calculGen.calculerNextGen(population);
         }
-        T meilleurIndividu = null;
-        int meilleurFitness = 0;
-        for (T individu : population) {
-            if (individu.evaluerFitness() > meilleurFitness) {
-                meilleurFitness = individu.evaluerFitness();
-                meilleurIndividu = individu;
-            }
-        }
+        T meilleurIndividu = Collections.max(population, Comparator.comparing(T::evaluerFitness));
         return meilleurIndividu.calculerSol();
     }
 
--- src/test/java/fr/umontpellier/iut/algogen/AlgoGenetiqueTest.java
+++ src/test/java/fr/umontpellier/iut/algogen/AlgoGenetiqueTest.java
@@ -1,12 +1,23 @@
 package fr.umontpellier.iut.algogen;
 
+import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import fr.umontpellier.iut.algogen.fabriques.CreationIndividuGDBHSmartCrossingSmartMutViaPermut;
+import fr.umontpellier.iut.algogen.fabriques.ICreator;
 import fr.umontpellier.iut.algogen.individus.GDBHSmartCrossingSmartMut;
+import fr.umontpellier.iut.algogen.individus.IIndividu;
 
 public class AlgoGenetiqueTest {
 
@@ -40,8 +51,35 @@ public class AlgoGenetiqueTest {
 
 		AlgoGenetique ag = new AlgoGenetique(in4, new CreationIndividuGDBHSmartCrossingSmartMutViaPermut());
 		assertDoesNotThrow(() -> ag.run(2, 30));
+	}
 
-		;
+	@Test
+	public void testRun_AvecMock_retourneMeilleurIndividu() {
+		Solution resultatAttendu = new Solution();
+		boolean[][] plateau = new boolean[][] { // Disposition des pièces :
+				{ false, true, false, false, false }, // l0: . o . . .
+				{ false, true, true, false, false }, // ,l1: . o o . .
+				{ false, false, true, true, false }, // ,l2: . . o o .
+				{ false, false, false, true, true }, // ,l3: . . . o o
+				{ false, false, false, false, true } // ,l4: . . . . o
+		};
+		Coord coordDepart = new Coord(0, 0);
+		int k = 8;
+		Instance instance = new Instance(plateau, coordDepart, k);
+		ICreator<?> mockICreator = Mockito.mock(ICreator.class);
+		AlgoGenetique<?> algoGenetique = new AlgoGenetique<>(instance, null, mockICreator);
+		IIndividu mockIIndividu1 = initMockIndividu(resultatAttendu, 50);
+		IIndividu mockIIndividu2 = initMockIndividu(new Solution(), 1);
+		when(mockICreator.creerPopInit(any(Instance.class), anyInt()))
+				.thenReturn(new ArrayList<>(Arrays.asList(mockIIndividu1, mockIIndividu2)));
+		assertSame(resultatAttendu, algoGenetique.run(2, 0));
+	}
+
+	private IIndividu initMockIndividu(Solution solution, int fitness) {
+		IIndividu mockIIndividu = Mockito.mock(IIndividu.class);
+		when(mockIIndividu.evaluerFitness()).thenReturn(fitness);
+		when(mockIIndividu.calculerSol()).thenReturn(solution);
+		return mockIIndividu;
 	}
 
 }
