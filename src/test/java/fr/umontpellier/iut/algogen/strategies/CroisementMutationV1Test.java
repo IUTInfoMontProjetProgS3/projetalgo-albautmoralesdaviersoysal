f18c874b43cc2f7f9a893d003fde81417d0f5ecd
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-12-05 22:08:57 +0100

    ✅ Mise à jour des tests : CroisementMutationV1Test

--- src/test/java/fr/umontpellier/iut/algogen/strategies/CroisementMutationV1Test.java
+++ src/test/java/fr/umontpellier/iut/algogen/strategies/CroisementMutationV1Test.java
@@ -13,11 +13,9 @@ import org.junit.Test;
 import fr.umontpellier.iut.algogen.Coord;
 import fr.umontpellier.iut.algogen.Instance;
 import fr.umontpellier.iut.algogen.individus.GDBHSimple;
-import fr.umontpellier.iut.algogen.individus.GDBHSmartCrossingSmartMut;
 
 public class CroisementMutationV1Test {
 
-	@Ignore
 	@Test
 	public void testCroisementPuisMutation() throws Exception {
 
@@ -72,36 +70,35 @@ public class CroisementMutationV1Test {
 		Coord sp4 = new Coord(0, 0);
 		int k4 = 4;
 		Instance in4 = new Instance(p4, sp4, k4);
-		GDBHSmartCrossingSmartMut individu = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu = new GDBHSimple(in4);
 		individu.trajet.clear();
 		individu.trajet.add('d');
 		individu.trajet.add('d');
 		individu.trajet.add('d');
 		individu.trajet.add('b');
-		GDBHSmartCrossingSmartMut individu_ = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu_ = new GDBHSimple(in4);
 		individu_.trajet.clear();
 		individu_.trajet.add('b');
 		individu_.trajet.add('d');
 		individu_.trajet.add('d');
 		individu_.trajet.add('b');
-		GDBHSmartCrossingSmartMut individu1 = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu1 = new GDBHSimple(in4);
 		individu1.trajet.clear();
 		individu1.trajet.add('d');
 		individu1.trajet.add('b');
 		individu1.trajet.add('d');
 		individu1.trajet.add('h');
 
-		ArrayList<Integer> scores = new ArrayList();
-		ArrayList<Integer> avg1 = new ArrayList();
-		ArrayList<Integer> avg2 = new ArrayList();
-		ArrayList<Integer> avg3 = new ArrayList();
-		ArrayList<GDBHSmartCrossingSmartMut> pop = new ArrayList(Arrays.asList(individu, individu1, individu_));
+		ArrayList<Integer> scores = new ArrayList<>();
+		ArrayList<Integer> avg1 = new ArrayList<>();
+		ArrayList<Integer> avg2 = new ArrayList<>();
+		ArrayList<Integer> avg3 = new ArrayList<>();
+		ArrayList<GDBHSimple> pop = new ArrayList<>(Arrays.asList(individu, individu1, individu_));
 
 		for (int i = 0; i < 30; i++) {
-
 			for (int j = 0; j < 30; j++) {
-				GDBHSmartCrossingSmartMut selected = (GDBHSmartCrossingSmartMut) new CroisementMutationV1(0.1)
-						.selectionRoulette(pop);
+				CroisementMutationV1<GDBHSimple> croisementMutationV1 = new CroisementMutationV1<>(0.1);
+				GDBHSimple selected = croisementMutationV1.selectionRoulette(pop);
 				scores.add(selected.evaluerFitness());
 			}
 			avg1.add(Collections.frequency(scores, 21));
@@ -139,36 +136,36 @@ public class CroisementMutationV1Test {
 		Coord sp4 = new Coord(0, 0);
 		int k4 = 4;
 		Instance in4 = new Instance(p4, sp4, k4);
-		GDBHSmartCrossingSmartMut individu = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu = new GDBHSimple(in4);
 		individu.trajet.clear();
 		individu.trajet.add('d');
 		individu.trajet.add('d');
 		individu.trajet.add('d');
 		individu.trajet.add('b');
-		GDBHSmartCrossingSmartMut individu_ = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu_ = new GDBHSimple(in4);
 		individu_.trajet.clear();
 		individu_.trajet.add('b');
 		individu_.trajet.add('d');
 		individu_.trajet.add('d');
 		individu_.trajet.add('b');
-		GDBHSmartCrossingSmartMut individu1 = new GDBHSmartCrossingSmartMut(in4);
+		GDBHSimple individu1 = new GDBHSimple(in4);
 		individu1.trajet.clear();
 		individu1.trajet.add('d');
 		individu1.trajet.add('b');
 		individu1.trajet.add('d');
 		individu1.trajet.add('h');
 
-		ArrayList<Integer> scores = new ArrayList();
-		ArrayList<Integer> avg1 = new ArrayList();
-		ArrayList<Integer> avg2 = new ArrayList();
-		ArrayList<Integer> avg3 = new ArrayList();
-		ArrayList<GDBHSmartCrossingSmartMut> pop = new ArrayList(Arrays.asList(individu, individu1, individu_));
+		ArrayList<Integer> scores = new ArrayList<>();
+		ArrayList<Integer> avg1 = new ArrayList<>();
+		ArrayList<Integer> avg2 = new ArrayList<>();
+		ArrayList<Integer> avg3 = new ArrayList<>();
+		ArrayList<GDBHSimple> pop = new ArrayList<>(Arrays.asList(individu, individu1, individu_));
 
+		CroisementMutationV1<GDBHSimple> croisementMutationV1 = new CroisementMutationV1<>(0.1);
 		for (int i = 0; i < 30; i++) {
 
 			for (int j = 0; j < 30; j++) {
-				GDBHSmartCrossingSmartMut selected = (GDBHSmartCrossingSmartMut) new CroisementMutationV1(0.1)
-						.selectionRoulette(pop);
+				GDBHSimple selected = croisementMutationV1.selectionRoulette(pop);
 				scores.add(selected.evaluerFitness());
 			}
 			avg1.add(Collections.frequency(scores, 21));
@@ -178,9 +175,9 @@ public class CroisementMutationV1Test {
 
 		assertTrue(calculateAverage(avg3) > 12);
 
-		ArrayList<GDBHSmartCrossingSmartMut> parents = new CroisementMutationV1(0.1).selectionParents(pop);
+		ArrayList<GDBHSimple> parents = croisementMutationV1.selectionParents(pop);
 
-		assertTrue(parents.size() == 2);
+		assertEquals(2, parents.size());
 
 	}
 
